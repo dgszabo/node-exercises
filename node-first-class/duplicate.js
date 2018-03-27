dd50426edf5ce4e055eefb581f042c96fe009b88
Daniel Szabo <dgszabo@gmail.com>
2018-03-27 13:24:50 -0700

    node first class - duplicate  rewritten

--- node-first-class/duplicate.js
+++ node-first-class/duplicate.js
@@ -2,23 +2,31 @@ const fs = require('fs');
 
 fs.readFile('./text.txt', (err, text) => {
     for(let i = 1; i <= 5; i++) {
+        console.log(`file ${i} started copying`)
         fs.writeFile(`./copy${i}.txt`, text, function(err) {
             if(err) {
                 console.log('something went wrong: ', err)
             }
         });
+        console.log(`file ${i} finished copying`)
     }
-    console.log('finished!')
+    console.log('copying finished!')
 });
 
-fs.readFile('./surprise.jpg', (err, text) => {
-    for(let i = 1; i <= 5; i++) {
-        fs.writeFile(`./copy${i}.jpg`, text, function(err) {
-            if(err) {
-                console.log('something went wrong: ', err)
-            }
-        });
-    }
-    console.log('finished!')
-});
+// fs.readFile('./surprise.jpg', (err, text) => {
+//     for(let i = 1; i <= 5; i++) {
+//         fs.writeFile(`./copy${i}.jpg`, text, function(err) {
+//             if(err) {
+//                 console.log('something went wrong: ', err)
+//             }
+//         });
+//     }
+//     console.log('finished!')
+// });
 
+var inText = fs.createReadStream('./surprise.jpg');
+
+for(let i = 1; i <= 5; i++) {
+    let outText = fs.createWriteStream(`./copy${i}.jpg`)
+    inText.pipe(outText);
+}
